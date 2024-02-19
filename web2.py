a82266249d0bc1b4715ae92a4ebba5ee0b651808
Maxim <maxim@redsquare.de>
2024-02-19 06:51:27 +0100

    Adding new items feature

--- web2.py
+++ web2.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-st.checkbox("Buy grocery.")
-
 for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="", placeholder="Add a new todo...")
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key= 'new_todo')
+
+st.session_state
